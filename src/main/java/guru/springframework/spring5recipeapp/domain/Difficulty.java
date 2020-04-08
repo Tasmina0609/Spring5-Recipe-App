d4ae27a3e2e3b78c40f5782a2004950bb7cb2e0d
Parsha Joarder <parshajoarder@gmail.com>
2020-04-08 15:10:48 +0400

    JPA enumerations

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring5recipeapp.domain;
+
+public enum Difficulty  {
+    EASY,MODERATE,KIND_OF_HARD,HARD
+}
--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -17,10 +17,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-//    private Difficulty difficulty;
 
     @Lob // binary large object field
     private Byte[] image;
+
+    @Enumerated(value=EnumType.STRING)  // store the String value of the enum in the database.
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL) // defines relationship  ( cascade - make recipe the owner )
     private Notes notes;
@@ -106,5 +108,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
