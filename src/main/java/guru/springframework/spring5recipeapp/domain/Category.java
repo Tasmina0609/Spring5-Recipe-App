3377dd7cdd5f36d384632d6f92715f55c439dea7
Parsha Joarder <parshajoarder@gmail.com>
2020-04-09 06:02:38 +0400

    Many To Many JPA Relationships,Join Table

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")  // from the JoinTable --> Set<Category> categories
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipe() {
+        return recipes;
+    }
+
+    public void setRecipe(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -27,8 +27,16 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL) // defines relationship  ( cascade - make recipe the owner )
     private Notes notes;
 
+    @ManyToMany  // using only this leads to different table
+    @JoinTable(name="recipe_category",
+               joinColumns = @JoinColumn(name="recipe_id"),
+               inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
+
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")  // mappedBy - removing the parent should also remove the child entities
     private Set<Ingredient> ingredients;
+
+
 
     public Long getId() {
         return id;
@@ -124,5 +132,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
